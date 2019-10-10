8c7b9478ef160ccc6977a67e02bcae218645c130
Jas Spencer <jas.spencer414@gmail.com>
2019-10-10 17:27:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,10 @@ def speak_to_grandma(phrase)
 if phrase == "Hi Nana, how are you?"
   return "HUH?! SPEAK UP, SONNY!"
 
+elsif phrase.upcase
+  return "NO, NOT SINCE 1938!" 
+  
+
 end
 end
 # Whatever you say to grandma, she should respond with
